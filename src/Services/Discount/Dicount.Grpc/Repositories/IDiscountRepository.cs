61320cfcbb4e8489b5cb49dde71b07ac24b851ea
Ryoma <ryoma76@gmail.com>
2021-08-16 12:42:02 +0200

    Discount Services GRPC Add

---
+++ src/Services/Discount/Dicount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Grpc.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon copuon);
+        Task<bool> UpdateDiscount(Coupon copuon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
