65cb42bfd70ab5c031c3a29e2ed3d9c9de59a0da
Ryoma <ryoma76@gmail.com>
2021-08-17 15:02:58 +0200

    Application Layer (DDD)

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException: ApplicationException
+    {
+        public ValidationException(): base("One or more validation failure have occured.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures.GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroups => failureGroups.Key, failureGroups => failureGroups.ToArray());
+        }
+
+        public IDictionary<string, string[]>Errors { get; }
+    }
+}
