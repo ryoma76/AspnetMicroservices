a23dbc7e44ba0c6dc9a10957a52003e313bd2e27
Ryoma <ryoma76@gmail.com>
2021-08-19 10:09:14 +0200

    Configure MassTransit-RabbitMq

--- src/Services/Basket/Basket.Api/Startup.cs
+++ src/Services/Basket/Basket.Api/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.Api.GrpcServices;
 using Basket.Api.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -28,17 +29,30 @@ namespace Basket.Api
                 option.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
-            
+
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
             services.AddScoped<DiscountGrpcService>();
-            services.AddControllers();
             
+            // MassTransit- Rabbitmq Configuration
+            services.AddMassTransit(
+                    config =>
+                    {
+                        config.UsingRabbitMq((ctx, cfg) =>
+                        {
+                            cfg.Host(Configuration["EventBusSetting:HostAddress"]);
+                        });
+                    }
+                );
+            services.AddMassTransitHostedService();
+
+            services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.Api", Version = "v1" });
             });
-           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
